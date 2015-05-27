fa28f158cf0fda8d75db4bc9dce8635541bbd950
Kalimaha <guido.barbaglia@gmail.com>
2015-05-27 15:53:12 +0200

    Method to invoke REST services. To be used by FAOSTAT.

--- js/WDSClient.js
+++ js/WDSClient.js
@@ -1,7 +1,4 @@
-/*global define*/
-define([
-    'jquery'
-], function ($) {
+define(['jquery', 'text!fenix_ui_common/resources/schemas/wds.json'], function ($, wds_schema) {
 
     'use strict';
 
@@ -33,7 +30,8 @@ define([
             cssFilename: '',
             nowrap: false,
             valuesIndex: 0,
-            json: JSON.stringify({query: ''})
+            json: JSON.stringify({query: ''}),
+            wds_schema: $.parseJSON(wds_schema),
         }, config);
 
         return this;
@@ -139,6 +137,68 @@ define([
             if (this.opts.datasource == undefined || this.opts.datasource == null)
                 throw 'Missing parameter "datasource" in the default configuration and in the configuration object.';
         }
+    };
+
+    wdsClient.prototype.wdsclient = function(rest_service_name, parameters, callback, url_root) {
+
+        /* Root URL. */
+        var url = url_root != null ? url_root : this.CONFIG.wds_root;
+        url += '/' + rest_service_name + '/';
+
+        /* Load REST definition. */
+        var rest_parameters = this.opts.wds_schema.properties[rest_service_name].properties;
+
+        try {
+
+            /* Check whether the CONFIG object contains all the required parameters for the REST. */
+            this.check_parameters(parameters, rest_parameters);
+
+            /* Create the URL by taking the parameters from the CONFIG object according to the JSON Schema definition. */
+            for (var i = 0 ; i < Object.keys(rest_parameters).length ; i++)
+                url += parameters[Object.keys(rest_parameters)[i]] + '/';
+
+            /* Define the HTTP method, GET by default, */
+            var method = this.opts.wds_schema.properties[rest_service_name].method;
+            method = method != null ? method : 'GET';
+
+            /* Call WDS. */
+            $.ajax({
+
+                type: method,
+                url: url,
+
+                success: function (response) {
+
+                    /* Cast response to JSON, if needed. */
+                    var json = response;
+                    if (typeof json == 'string')
+                        json = $.parseJSON(response);
+
+                    /* Invoke user's callback. */
+                    if (callback != null)
+                        callback(json);
+
+                },
+
+                /* Default error handling. */
+                error: function(a) {
+                    alert(a.responseText);
+                }
+
+            });
+
+        } catch (e) {
+
+            alert(e);
+
+        }
+
+    };
+
+    wdsClient.prototype.check_parameters = function(parameters, rest_parameters) {
+        for (var i = 0 ; i < Object.keys(rest_parameters).length ; i++)
+            if (parameters[Object.keys(rest_parameters)[i]] == undefined)
+                throw translate.missing_parameter + Object.keys(rest_parameters)[i];
     };
 
     return wdsClient;
