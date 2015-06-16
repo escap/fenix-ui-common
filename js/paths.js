45e98a0adcaa77d81d4a55d98d9570e34a49efda
danielefenix <daniele.salvatore@fao.org>
2015-06-16 14:09:11 +0200

    FAOSTATCommon in paths.js

--- js/paths.js
+++ js/paths.js
@@ -1,13 +1,16 @@
 /*global define*/
 define(function () {
 
+    'use strict';
+
     var config = {
 
         paths : {
             'fx-common' : './',
             // third party libs
             jquery: "//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min",
-            amplify: "//fenixapps.fao.org/repository/js/amplify/1.1.2/amplify.min"
+            amplify: "//fenixapps.fao.org/repository/js/amplify/1.1.2/amplify.min",
+            faostat_commons : "FAOSTATCommons"
         },
 
         shim: {
