227189f009e84ba25b165df63af655386d7c39f2
danielefenix <daniele.salvatore@fao.org>
2015-07-23 16:37:13 +0200

    Authentication Manager: reset form after login and logout

--- js/AuthManager.js
+++ js/AuthManager.js
@@ -10,8 +10,8 @@ define([
     'use strict';
 
     var defaults = {
-    	onLogin: null,
-    	onLogout: null
+        onLogin: null,
+        onLogout: null
     };
 
     var s = {
@@ -21,19 +21,19 @@ define([
         EMAIL_LOGIN: '#fx-login-form-inputEmail',
         PASSWORD_LOGIN: '#fx-login-form-inputPassword',
         ERROR_CONTAINER_LOGIN: '#fx-login-form-error-container',
-        SUBMIT_LOGIN : "#fx-login-form-submit",
+        SUBMIT_LOGIN: "#fx-login-form-submit",
         //logout
         FORM_LOGOUT: '#fx-logout-form',
         MODAL_LOGOUT: '#fx-logout-modal',
         ERROR_CONTAINER_LOGOUT: '#fx-logout-form-error-container',
-        SUBMIT_LOGOUT : "#fx-logout-form-submit",
-        CANCEL_LOGOUT : "#fx-logout-form-cancel",
+        SUBMIT_LOGOUT: "#fx-logout-form-submit",
+        CANCEL_LOGOUT: "#fx-logout-form-cancel",
         STORAGE_KEY: "fx.auth.user"
     };
 
     function AuthManager(opts) {
-    	//TODO extend defaults..
-    	this.opts = $.extend(true, defaults, opts);
+        //TODO extend defaults..
+        this.opts = $.extend(true, defaults, opts);
 
         this.users = JSON.parse(AuthUsers);
         $('body').append(template);
@@ -47,7 +47,7 @@ define([
         this.$formLogin = $(s.FORM_LOGIN);
         this.$modalLogin = $(s.MODAL_LOGIN);
         this.$emailLogin = $(s.EMAIL_LOGIN);
-        this.$submitLogin = $(s.SUBMIT_LOGIN);        
+        this.$submitLogin = $(s.SUBMIT_LOGIN);
         this.$passwordLogin = $(s.PASSWORD_LOGIN);
         this.$errorContainerLogin = $(s.ERROR_CONTAINER_LOGIN);
         //logout
@@ -70,7 +70,7 @@ define([
         });
 
         this.$submitLogin.on('click', function () {
-            if ( self.$formLogin[0].checkValidity()){
+            if (self.$formLogin[0].checkValidity()) {
                 self._authenticate();
             }
         });
@@ -90,35 +90,42 @@ define([
 
     AuthManager.prototype._logout = function () {
 
+        this._resetLoginForm();
         this.$modalLogout.modal('hide');
         console.warn("Logout success.");
-        console.warn("Removing authenticated user details with key: "+s.STORAGE_KEY);
-        amplify.store.sessionStorage(s.STORAGE_KEY, '' );
+        console.warn("Removing authenticated user details with key: " + s.STORAGE_KEY);
+        amplify.store.sessionStorage(s.STORAGE_KEY, '');
         amplify.publish('fx.auth.logout');
-        if(this.opts.onLogout)
-        	this.opts.onLogout();
+        if (this.opts.onLogout)
+            this.opts.onLogout();
     };
 
     AuthManager.prototype._authenticate = function () {
 
         var email = this.$emailLogin.val(),
             password = this.$passwordLogin.val(),
-            user= this.users[email];
+            user = this.users[email];
 
-        if(user && user.password === password)
+        if (user && user.password === password)
             this._onAuthenticationSuccess(user);
         else
             this._onAuthenticationError();
     };
 
+    AuthManager.prototype._resetLoginForm = function () {
+        this.$emailLogin.val('');
+        this.$passwordLogin.val('');
+    };
+
     AuthManager.prototype._onAuthenticationSuccess = function (user) {
 
+        this._resetLoginForm();
         this.$modalLogin.modal('hide');
-        console.warn("Login success. Storing authenticated user details with key: "+s.STORAGE_KEY);
-        amplify.store.sessionStorage(s.STORAGE_KEY, user );
+        console.warn("Login success. Storing authenticated user details with key: " + s.STORAGE_KEY);
+        amplify.store.sessionStorage(s.STORAGE_KEY, user);
         amplify.publish('fx.auth.login', amplify.store.sessionStorage(s.STORAGE_KEY));
-        if(this.opts.onLogin)
-        	this.opts.onLogin(user);
+        if (this.opts.onLogin)
+            this.opts.onLogin(user);
     };
 
     AuthManager.prototype._onAuthenticationError = function () {
@@ -126,12 +133,12 @@ define([
         this.$errorContainerLogin.html("Invalid login! Email and password do not match.");
     };
 
-	AuthManager.prototype.isLogged = function () {
-		return !!amplify.store.sessionStorage(s.STORAGE_KEY);
+    AuthManager.prototype.isLogged = function () {
+        return !!amplify.store.sessionStorage(s.STORAGE_KEY);
     };
 
-    AuthManager.prototype.getCurrentUser = function() {
-    	return amplify.store.sessionStorage(s.STORAGE_KEY) || false;
+    AuthManager.prototype.getCurrentUser = function () {
+        return amplify.store.sessionStorage(s.STORAGE_KEY) || false;
     };
 
     return AuthManager;
