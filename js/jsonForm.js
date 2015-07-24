ecbeb540624568668508197390fa7c252c6efd0f
stefanocudini <stefano.cudini@gmail.com>
2015-07-24 10:36:23 +0200

    added js html jsonForm storeForm

---
+++ js/jsonForm.js
@@ -0,0 +1,95 @@
+/*
+
+USAGE:
+	jsonForm('#form-contact', 'json/schema.contact.json');
+
+	//new dom element
+	var form$ = $('<form>').prependTo('body');
+	jsonForm(form$, 'json/schema.contact.json');
+
+*/
+define([
+	'require','jquery','underscore','handlebars','jsoneditor',
+	//'fmdTheme',
+	'text!html/jsonForm.html'
+], function (require, $, _, Handlebars, JSONEditor, 
+	//FMDTheme,
+	jsonFormHtml) {
+
+	var tmplJsonForm = Handlebars.compile(jsonFormHtml);
+
+	function jsonForm(target, opts) {
+
+		opts = opts || {};
+
+		var self = this;
+
+		self.target = (target instanceof jQuery) ? target : $(target);
+
+
+		self.opts = _.defaults(opts, {
+			template: 'handlebars',
+			theme: 'bootstrap3',		
+			iconlib: 'fontawesome4',
+			//TODO languages using module nls/jsoneditor_errors.js
+
+			ajax: true,
+			editable: false,
+			disable_collapse: true,
+			disable_edit_json: true,
+			disable_properties: true,
+			disable_array_reorder: true,
+
+			values: {},
+			schema: _.isString(opts.schema) ? {$ref: require.toUrl(opts.schema)} : opts.schema,
+			disabled: [],
+
+			tmpl: {
+				idform: self.target.attr('id'),
+				submit: 'Save',
+				reset: 'Cancel'
+			},
+			//ballbacks
+			onChange: $.noop,
+			onSubmit: $.noop
+		});
+
+		if(!_.isUndefined(opts.editable))
+			self.opts = _.extend(self.opts, {
+				editable:              opts.editable,
+				disable_collapse:     !opts.editable,
+				disable_edit_json:    !opts.editable,
+				disable_properties:   !opts.editable,
+				disable_array_reorder:!opts.editable
+			});
+
+		self.target.html( tmplJsonForm(self.opts.tmpl) );
+
+		self.editor = new JSONEditor(self.target.find('.form-wrapper-content')[0], self.opts);
+
+		if(self.opts.disabled.length>0)
+			_.each(self.opts.disabled, function(key) {
+				self.editor.getEditor('root.'+key).disable();
+			});
+
+		self.editor.on('change', _.after(2, function(e) {
+			self.opts.onChange.call(self, self.editor.getValue() );
+		}) );
+
+		if(!_.isEmpty(self.opts.values))
+			self.editor.setValue(self.opts.values);
+
+
+		self.target.find('.form-wrapper-submit').on('click', function(e) {
+			e.preventDefault();
+			self.opts.onChange.call(self, self.editor.getValue() );
+			self.opts.onSubmit.call(self, self.editor.getValue() );
+		});
+
+		return self;
+	};
+
+	return function (target, schemaUrl, opts) {
+		return new jsonForm(target, schemaUrl, opts);
+	}
+});
